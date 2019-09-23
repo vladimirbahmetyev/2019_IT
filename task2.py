394b0dd96cc3901946736153f1d59c947ed34fe4
Vladimir <vladimir240298perm@gmail.com>
2019-09-23 15:50:11 +0300

    upload task2

--- task2.py
+++ task2.py
@@ -1,36 +1,38 @@
+from math import exp
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-def add_noise(img, rate=5):
-    img[::rate, ::rate, :] = 1
-    return
+def gaussian(x, mu, sigma): return exp(-(((x - mu) / sigma) ** 2) / 2.0)
 
 
-def get_kernel(windos_size):
-    kernel = np.ones((windos_size, windos_size))
-    kernel/=kernel.sum()
-    return kernel
+def gaussKernel(kernelRadius, sigma):
+    hKernel = [gaussian(x, kernelRadius, sigma) for x in range(2 * kernelRadius + 1)]
+    vKernel = [x for x in hKernel]
+    kernel = [[xh * xv for xh in hKernel] for xv in vKernel]
+    kernelSum = sum([sum(row) for row in kernel])
+    kernel = [[x / kernelSum for x in row] for row in kernel]
+    return np.array([np.array(xi) for xi in kernel])
 
 
-def filter(img, window_size=3):
-    img2 = np.zeros_like(img)
-    kernel = get_kernel(window_size)
-    p = window_size//2
-    for k in range(img.shape[2]): # foreach color channel
-        for i in range(p, img.shape[0]-p): # foreach row
-            for j in range(p, img.shape[1]-p): # foreach column
-                window = img[i-p:i+p+1, j-p:j+p+1, k]
-                img2[i,j,k] = (kernel*window).sum()
-    return img2
+def filterGauss(img, matrixSize):
+    imgGauss = np.zeros_like(img)
+    kernel = gaussKernel(matrixSize // 2, 10)
+    padding = matrixSize // 2
+    for i in range(matrixSize // 2, img.shape[0] - matrixSize // 2):
+        for j in range(matrixSize // 2, img.shape[1] - matrixSize // 2):
+            for c in range(img.shape[2]):
+                conv = img[i - padding:i + padding + 1, j - padding:j + padding + 1, c] * kernel
+                imgGauss[i, j, c] = conv.sum()
+
+    return imgGauss
 
 
 def main():
-    img = plt.imread("img.png")[:, :, :3]
-    add_noise(img)
-    img2 = filter(img)
+    img = plt.imread("go.png")[:, :, :3]
+    img2 = filterGauss(img, 22)
 
-    fig, axs = plt.subplots(1,2)
+    fig, axs = plt.subplots(1, 2)
     axs[0].imshow(img)
     axs[1].imshow(img2)
     plt.show()
